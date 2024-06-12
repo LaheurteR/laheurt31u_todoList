6de2cdd9e28119642127a0b33150dc1c49723430
LaheurteR <97089676+LaheurteR@users.noreply.github.com>
2024-06-12 14:33:08 +0200

    debut

---
+++ todo_list/lib/todo_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TodoListApp extends StatelessWidget {
+  const TodoListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Todo list', home: Scaffold(
+    ));
+  }
+}
