bc8c369e5b9c4b26e55c5920c48fffb86cfab1fe
LaheurteR <97089676+LaheurteR@users.noreply.github.com>
2024-06-12 15:50:09 +0200

    debut etape4

--- todo_list/lib/todo_list_app.dart
+++ todo_list/lib/todo_list_app.dart
@@ -1,26 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list/tasks_master.dart';
 
 class TodoListApp extends StatelessWidget {
   const TodoListApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Todo list',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Center(
-              child: Text("ToDo List"),
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: (){
-
-            },
-            backgroundColor: Colors.blue,
-            child:
-            const Icon(Icons.add),
-          ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-    ));
+    return TasksMaster();
   }
 }
